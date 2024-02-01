5406e51a169f80edc5d364e157ae3201dbac029e
Giacomo Angione <angi1g@gmail.com>
2024-02-01 15:20:52 +0100

    Ch. 5.2

---
+++ ToDoList/DetailedView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailedView.swift
+//  ToDoList
+//
+//  Created by Giacomo on 30/01/2024.
+//
+
+import SwiftUI
+
+struct DetailedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailedView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailedView()
+    }
+}
