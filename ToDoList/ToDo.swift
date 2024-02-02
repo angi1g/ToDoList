b77ce2083a32dba6005d74269edf9f18d4900458
Giacomo Angione <angi1g@gmail.com>
2024-02-02 14:16:07 +0100

    Ch 5.8.1

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Barbara on 02/02/24.
+//
+
+import Foundation
---
+++ ToDoList/ToDosViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDosViewModel.swift
+//  ToDoList
+//
+//  Created by Barbara on 02/02/24.
+//
+
+import Foundation
